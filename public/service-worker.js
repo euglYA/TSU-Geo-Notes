b1f4724fcf13ed66aad1ae5140432d3d945b5463
euglYA <90235668+euglYA@users.noreply.github.com>
2025-05-08 22:03:07 +0700

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,11 +2,11 @@ self.addEventListener('install', (e) => {
     e.waitUntil(
         caches.open('geo-notes').then((cache) => {
             return cache.addAll([
-                '/',
-                '/index.html',
-                '/manifest.json',
-                '/icons/icon-192.png',
-                '/icons/icon-512.png',
+                'index.html',
+                'public/service-worker.js',
+                'public/manifest.json',
+                'public/icons/icon-192.png',
+                'public/icons/icon-512.png',
                 'https://unpkg.com/leaflet/dist/leaflet.css',
                 'https://unpkg.com/leaflet/dist/leaflet.js'
             ]);
